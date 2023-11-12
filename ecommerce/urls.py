fcf22529650c885eceb42be1cc7b84cbd84f293f
Mohammed Abdalla <moh.khatim.dev@gmail.com>
2023-11-13 01:10:55 +0400

    Register Brand, Category and Product models to admin site add CategoryViewSet to get list of categories

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from product import models
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from product import views
 
-admin.site.register(models.Brand)
-admin.site.register(models.Category)
-admin.site.register(models.Product)
+router = DefaultRouter()
+router.register(f"category", views.CategoryViewSet)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from product import models
+from product.serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ViewSet):
+    queryset = models.Category.objects.all()
+
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
